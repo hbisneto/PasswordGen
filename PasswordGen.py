645d426ad511a1c73658548bf65bd4bf98fd7d82
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2021-05-13 05:54:12 -0300

    PasswordGen

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,3 +1,5 @@
+# PasswordGen (PasswordGen)
+
 import random
 import os
 from datetime import date
@@ -7,16 +9,14 @@ from string import ascii_letters
 
 AnoAtual = date.today().year
 SoftwareName = "PasswordGen"
-Version = "1.0"
+Version = "1.0.1"
 CopyrightName = "Heitor Bisneto."
 PassArray = []
 NoteProcessor = []
 SystemLocation = os.getcwd()
-SaveFolder = SystemLocation + '/Saved/'
-NotesFolder = SystemLocation + '/Saved/'
+SaveFolder = SystemLocation + '/PasswordRepo/'
 Extension = ['.txt']
 
-NomeNota = str()
 FileName = str()
     
 print("="*80)
@@ -30,6 +30,43 @@ if AnoAtual == 2021:
 else:
     print("Copyright © 2021 -", AnoAtual, "|", CopyrightName, "All rights reserved.")
 print()
+
+def PrepararSistema():
+    try:
+        print("="*80)
+        print(f'>> Verificação de arquivos do sistema {SoftwareName} <<')
+        print("="*80)
+        print(f'>> Verificando pasta: "{SaveFolder}"...')
+        os.mkdir(SaveFolder)
+        print(f'>> Pasta "{SaveFolder}" criada')
+        print()
+    except OSError:
+        print(f'>> Status: Pasta "{SaveFolder}" configurada!')
+        print()
+
+
+def Menu():
+    
+    print("=" * 80)
+    print('>> Menu <<')
+    print("=" * 80)
+
+    print("| 1. App() | >> Para abrir o app: Permite criar múltiplas senhas com o mesmo número de caracteres")
+    print("| 2. QuickApp() | >> Para abrir o modo rápido: Cria uma senha")
+    print("| 3. Save() | >> Para salvar senhas geradas: Salva as senhas geradas em um arquivo '.txt'")
+    print("=" * 80)
+    print(">> Nos dois primeiros módulos, o número de caracteres é escolhido pelo usuário. <<")
+    print("=" * 80)
+    MenuNumber = int(input(">> Digite o número da opção desejada: "))
+
+    if MenuNumber == 1:
+        App()
+    elif MenuNumber == 2:
+        QuickApp()
+    elif MenuNumber == 3:
+        Save()
+    else:
+        print(">> Opção inválida")
 
 def App():
     print("=" * 80)
@@ -50,7 +87,6 @@ def App():
         print(f'> Sua {Counter}º senha é: {Password}')
     print("=" * 80)
     print()
-    Menu()
 
 def QuickApp():
     print("=" * 80)
@@ -67,141 +103,20 @@ def QuickApp():
     print(f'> Sua nova senha é: {Password}')
     print("=" * 80)
     print()
-    Menu()
-
-def SystemPrepare():
-    try:
-        print("="*80)
-        print(f'>> Verificação de arquivos do sistema {SoftwareName} <<')
-        print("="*80)
-        print(f'>> Verificando pasta: "{SaveFolder}"...')
-        os.mkdir(SaveFolder)
-        print(f'>> Pasta "{SaveFolder}" criada')
-        print()
-    except OSError:
-        print(f'>> Status: Pasta "{SaveFolder}" configurada!')
-        print()
-
-def SalvarNota():
-    Count = 0
-    NewLine = "\n"
-    
-    print("="*80)
-    print(f'>> Salvar Nota: <<')
-    print("="*80)
-    NomeNota = str(input(">> Nome da Nota: "))
-    print("="*80)
-    print(f'>> Extensão do arquivo: <<')
-    print("="*80)
-
-    for Format in Extension:
-        Count += 1
-        print(f'{Count} {Format}')
-    print("="*80)
-    Opc = int(input(">> Escolha a extensão da Nota: "))
-
-    if Extension[Opc - 1] == ".rtf":
-        NotaExport = f'{NotesFolder}{NomeNota}{Extension[Opc-1]}'
-        NomeNota = f'{NomeNota}{Extension[Opc-1]}'
-        
-        RTFLib.FileName = NotaExport
-        RTFLib.NomeNota = NomeNota
-        RTFLib.SaveRTF()
-    else:
-        try:
-            f = open(f'{NotesFolder}{NomeNota}{Extension[Opc-1]}', 'x')
-            print(f'>> Nota salva como "{NomeNota}{Extension[Opc-1]}"')
-            for Writer in NoteProcessor:
-                f.write(Writer + NewLine)
-            print('>> Digite "App()" para executar o programa novamente')
-
-        except:
-            print("="*80)
-            print(f'>> Arquivo já existente: <<')
-            print('>> Gostaria de sobreescrever?')
-            print("="*80)
-            print('1. Sim')
-            print('2. Não')
-            print("="*80)
-            Confirm = int(input(f'>> Digite o número da opção: '))
-
-            if Confirm == 1:
-                f = open(f'{NotesFolder}{NomeNota}{Extension[Opc-1]}', 'w')
-                print(f'>> Nota salva como "{NomeNota}{Extension[Opc-1]}"')
-                for Writer in NoteProcessor:
-                    f.write(Writer + NewLine)
-                print('>> Digite "App()" para executar o programa novamente')
-            else:
-                print(">> A Nota não foi salva!")
-                App()
 
 def Save():
-    NewLine = "\n"
-    
-    print("="*80)
-    print(f'[MyNotes] - Exportar para RTF Document...')
-    print("="*80)
+    FileName = str(input("Digite o nome do arquivo: "))
+    print(">> Arquivo salvo com sucesso!")
+    f = open(f'{SaveFolder}{FileName}.pwd', 'w')
+    f.write(f'>> [{SoftwareName}] - Arquivo de senha\n\n')
+    Count = 0
+    for PassCodes in PassArray:
+        Count += 1
+        f = open(f'{SaveFolder}{FileName}.pwd', 'a')
+        f.write(f'{Count} - {PassCodes}\n')
+        f.close()
+    PassArray.clear()
 
-    NewFile = str(input("Insira um nome: "))
-    
-    try:
-        f = open(f'{NotesFolder}{NewFile}.txt', 'x')
-        print(f'>> Nota salva como "{NomeNota}"')
-        f.write(f'>> PasswordGen: Abaixo a lista de senhas geradas <<\n\n')
-        for Writer in CreateNote.NoteProcessor:
-            f.write(f'{Writer}\{NewLine}')
-        f = open(f'{FileName}', 'a')
-        f.write("}")
-        print('>> Digite "App()" para executar o programa novamente')
-
-    except:
-        print("="*80)
-        print(f'>> Arquivo já existente: <<')
-        print('>> Gostaria de sobreescrever?')
-        print("="*80)
-        print('1. Sim')
-        print('2. Não')
-        print("="*80)
-        Confirm = int(input(f'>> Digite o número da opção: '))
-
-        if Confirm == 1:
-            f = open(f'{NotesFolder}{FileName}', 'w')
-            print(f'>> Nota salva como "{NomeNota}"')
-            f.write(f'>> PasswordGen: Abaixo a lista de senhas geradas <<\n\n')
-            for Writer in CreateNote.NoteProcessor:
-                f.write(f'{Writer}\{NewLine}')
-            f = open(f'{FileName}', 'a')
-            f.write("}")
-            print('>> Digite "App()" para executar o programa novamente')
-        else:
-            print(">> A Nota não foi salva!")
-            App()
-            
+PrepararSistema() 
+while True:
     Menu()
-
-def Menu():
-    
-    print("=" * 80)
-    print('>> Menu <<')
-    print("=" * 80)
-
-    print("| 1. App() | >> Para abrir o app: Permite criar múltiplas senhas com o mesmo número de caracteres")
-    print("| 2. QuickApp() | >> Para abrir o modo rápido: Cria uma senha")
-    print("| 3. Save() | >> Para salvar senhas geradas: Salva as senhas geradas em um arquivo '.txt'")
-    print("=" * 80)
-    print(">> Nos dois módulos, o número de caracteres é escolhido pelo usuário. <<")
-    print("=" * 80)
-
-    MenuNumber = int(input(">> Digite o número da opção desejada: "))
-
-    if MenuNumber == 1:
-        App()
-    elif MenuNumber == 2:
-        QuickApp()
-    elif MenuNumber == 3:
-        Save()
-        #SalvarNota()
-    else:
-        print(">> Opção inválida")
-
-Menu()
